f3c4cc3cf6a6143cc32ccea595dcd17d55222691
HeSciheng <sh53@Illinois.edu>
2021-02-18 21:04:32 +0800

    commit a new file

---
+++ code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+@Project: pycharm project
+@File   : 992.py
+@Author : 91317
+@Time   : 2021/2/9 
+"""
+
+if __name__ == '__main__':
+    print('first line')
